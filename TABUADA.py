1837ed1db329578277c45f646c04e2fe952f09af
eversonbicalho <everson10bicalho@yahoo.com.br>
2022-05-14 18:04:25 -0300

    comitando Tabuada para o Github

---
+++ TABUADA.py
@@ -0,0 +1,31 @@
+import time
+
+print('')
+print('************** TABUADA **************')
+print('')
+
+valor=int(input('Qual tabuada deseja ser exibida? (Digite um numero):'))
+print('montando..... 5seg')
+time.sleep(1)
+print('montando.... 4seg')
+time.sleep(1)
+print('montando... 3seg')
+time.sleep(1)
+print('montando.. 2seg')
+time.sleep(1)
+print('montando. 1seg')
+time.sleep(1)
+
+print('')
+
+
+print(valor,'x  1 =',valor*1)
+print(valor,'x  2 =',valor*2)
+print(valor,'x  3 =',valor*3)
+print(valor,'x  4 =',valor*4)
+print(valor,'x  5 =',valor*5)
+print(valor,'x  6 =',valor*6)
+print(valor,'x  7 =',valor*7)
+print(valor,'x  8 =',valor*8)
+print(valor,'x  9 =',valor*9)
+print(valor,'x 10 =',valor*10)
